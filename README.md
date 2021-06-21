48bc41ccc14114b2a983911bce76392a2809b95e
ojudsonleo <ojudsonleo@gmail.com>
2021-06-21 09:38:09 +0530

    setuped files

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # setup:
-    $pip install django
-    $pip install pillow
-    $cd ModlePro
-    $python manage.py migrate
+
+```bash
+    pip install django
+    pip install pillow
+    cd ModlePro
+    python manage.py migrate
+```
 
 # run your server
     $python manage.py runserver 5050
--- static/setup.py
+++ static/setup.py
@@ -1,13 +1,35 @@
 import os
 
-try:
-    os.system("pip install pillow")
-    os.system("pip install django")
-    os.system("git clone https://github.com/ojudsonleo/Modle.git")
-    
+print("clean Setup")
+print("Custom Setup")
+i = input("$")
+if i == "help":
+    print("clean Setup")
+    print("Custom Setup")
 
-except:
-    os.system("pip install python-git")
-    os.system("pip install pillow")
-    os.system("pip install django")
-    os.system("git clone https://github.com/ojudsonleo/Modle.git")
+if i == "clean Setup":
+    try:
+        os.system("pip install django")
+        os.system("pip install pillow")
+        os.system("git clone https://github.com/ojudsonleo/Modle.git")
+
+        os.system("cd ModlePro")
+        os.system("python manage.py migrate")
+        os.system("python manage.py runserver")
+    except:
+        os.system("pip install python-git")
+        os.system("pip install pillow")
+        os.system("pip install django")
+        os.system("git clone https://github.com/ojudsonleo/Modle.git")
+if i == "Custom Setup":
+    c = input("$")
+    if c == "help":
+        print("conda Setup")
+        print("pip Setup")
+    if c == "conda setup":
+        os.system("conda install -c anaconda django")
+        os.system("conda install -c anaconda pillow")
+        os.system("git clone https://github.com/ojudsonleo/Modle.git")
+        os.system("cd ModlePro")
+        os.system("python manage.py migrate")
+        os.system("python manage.py runserver")
