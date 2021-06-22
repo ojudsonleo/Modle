6cf0303023c910079f9a418af873934ada2b2286
ojudsonleo <ojudsonleo@gmail.com>
2021-06-22 14:59:46 +0530

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Blog.as_view(), name="blog")
+    path('', Blog.as_view(), name="blog"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail")
 ]
